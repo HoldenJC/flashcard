3dccae3a46bef51e12c6c26dfdf358d199b553bf
Holden Clark and Na Hyung Choi <na@>
2019-06-04 16:54:09 -0700

    add highlight feature to card 1 when clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-  $(".q1").click(function () {
-    $(".q1 .answer").toggle();
-    $(".q1 .question").toggle();
+  $("#q1").click(function () {
+    $("#q1 .answer").toggle();
+    $("#q1 .question").toggle();
+    $('.card#q1').addClass("text-white bg-success");
   });
   $(".q2").click(function () {
     $(".q2 .answer").toggle();
@@ -10,5 +11,17 @@ $(document).ready(function () {
   $(".q3").click(function () {
     $(".q3 .answer").toggle();
     $(".q3 .question").toggle();
+  });
+  $(".q4").click(function () {
+    $(".q4 .answer").toggle();
+    $(".q4 .question").toggle();
+  });
+  $(".q5").click(function () {
+    $(".q5 .answer").toggle();
+    $(".q5 .question").toggle();
+  });
+  $(".q6").click(function () {
+    $(".q6 .answer").toggle();
+    $(".q6 .question").toggle();
   });
 });
