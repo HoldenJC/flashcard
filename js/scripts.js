da78a50dbdd396f86da5ae16259878995709f25e
Holden Clark and Na Hyung Choi <na@>
2019-06-04 13:42:42 -0700

    add 3 more question boxes, fix small formatting errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
   $(".q1").click(function () {
-    $(".answer").toggle();
-    $(".question").toggle();
+    $(".q1 .answer").toggle();
+    $(".q1 .question").toggle();
+  });
   $(".q2").click(function () {
-    $(".answer").toggle();
-    $(".question").toggle();
-    });
+    $(".q2 .answer").toggle();
+    $(".q2 .question").toggle();
+  });
+  $(".q3").click(function () {
+    $(".q3 .answer").toggle();
+    $(".q3 .question").toggle();
   });
 });
