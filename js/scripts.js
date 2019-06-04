44e3fb88c227c92fbf304bd2561512a628b41aa8
Holden Clark and Na Hyung Choi <holden@>
2019-06-04 12:56:13 -0700

    make placeholder flash cards that fade in and out

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".q1").click(function () {
+    $(".answer").fadeToggle();
+    $(".question").fadeToggle();
+  $(".q2").click(function () {
+    $(".answer").toggle();
+    $(".question").toggle();
+    });
+  });
+});
